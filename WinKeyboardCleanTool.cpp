6d20c408e3509784856c0f1066a13bc60c142dc3
1337 <35432275+decimaldreamer@users.noreply.github.com>
2024-10-26 01:14:03 +0300

    Add files via upload

---
+++ WinKeyboardCleanTool.cpp
@@ -0,0 +1,33 @@
+﻿#include <windows.h>
+#include <iostream>
+
+HHOOK keyboardHook;
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode >= 0) {
+        if (wParam == WM_KEYDOWN) {
+            return 1;
+        }
+    }
+    return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
+}
+void Cleanup() {
+    UnhookWindowsHookEx(keyboardHook);
+}
+
+int main() {
+    keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+
+    if (!keyboardHook) {
+        std::cerr << "Hook Failed Error Code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Your keyboard locked now. You can start cleaning, after you done just simply close the program." << std::endl;
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    Cleanup();
+    return 0;
+}
